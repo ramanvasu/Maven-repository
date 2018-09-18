c4a765fd35db2bbe1f53a9e77f8a27ca96496254
Sri Venkat <dgl.sri.tech@gmail.com>
2018-09-17 22:29:26 -0400

    On branch master

---
+++ SPboot/ContractClients/src/main/java/com/fnu/ContractClients/ContractClientsApplication.java
@@ -0,0 +1,12 @@
+package com.fnu.ContractClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractClientsApplication.class, args);
+	}
+}
---
+++ SPboot/contractclient/src/main/java/com/fnu/ContractClient/ContractClientApplication.java
@@ -0,0 +1,12 @@
+package com.fnu.ContractClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractClientApplication.class, args);
+	}
+}
